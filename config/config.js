4f0280f43711e15e4ec5d90a748e24fc0c66911f
jangdu <jjd0324@gmail.com>
2023-06-26 17:07:55 +0900

    fix: .env USERNAME을 DBUSERNAME으로 변경

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.USERNAME,
+    username: process.env.DBUSERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     host: process.env.HOST,
