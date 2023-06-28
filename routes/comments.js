49265377fb24259058751cc9c636e2160f8ac117
Lee Eunchan <ym4315@nate.com>
2023-06-28 16:42:01 +0900

    중간업로드

--- routes/comments.js
+++ routes/comments.js
@@ -1,26 +1,46 @@
 const express = require('express');
 const router = express.Router();
-const { Comment } = require('../models');
-const { User } = require('../models');
+const { Comment, Post, User } = require('../models');
 const { Op } = require('sequelize');
+
 //import * as tweetController from '../controller/tweet.js';
 
-router.get('/', (req, res, next) => {
-  res.send('comments router');
-});
+// router.get('/', (req, res, next) => {
+//   res.send('comments router');
+// });
 
 //댓글 생성
 router.post('/', async (req, res) => {
-  const { content } = req.body;
+  const { content, userId } = req.body;
+  // if (!userId) {
+  //   res.status(400).json({
+  //     errorMessage: '로그인이 필요한 기능입니다.',
+  //   });
+  // }
 
-  await Comment.create({
-    content: content,
-  });
+  try {
+    const user = await User.findOne({
+      attributes: [`id`, `nickname`, `email`, 'password'],
+      where: { id: userId },
+    });
+    if (!user) {
+      res.status(400).json({
+        errorMessage: '존재하지 않는 사용자 입니다.',
+      });
+    }
+    const comment = await Comment.create({
+      content: content,
+      userId: userId,
+    });
 
-  res.status(200).json({ content: content });
+    res.status(200).json({ comment });
+  } catch {
+    // console.error(error);
+    // res.status(500).json({
+    //   errorMessage: '서버 오류',
+    // });
+  }
 });
-
-module.exports = router;
 
 //댓글 조회
 router.get('/', async (req, res) => {
@@ -44,6 +64,7 @@ router.put('/:id', async (req, res) => {
   const comment = await Comment.findOne({ where: { id } });
   await comment.update({
     content: content,
+    updateAt: Date.now,
   });
 
   res.status(200).json({ content: content });
@@ -53,10 +74,15 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
   const { content } = req.body;
-  const comment = await Comment.findOne({ where: { id } });
-  await comment.destroy({ id });
+  const comment = await Comment.findOne({ id });
+  if (!comment) {
+    res.status(400).json({
+      errorMessage: '댓글이 삭제되었거나 존재하지 않습니다.',
+    });
+  }
+  await comment.destroy({});
 
-  res.status(200).json({ content: content });
+  res.status(200).json({ success: '댓글이 삭제되었습니다.' });
 });
 
 module.exports = router;
