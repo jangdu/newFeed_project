0f3f437075a65d8c4df343fe4f551ff482a0807b
Lee Eunchan <ym4315@nate.com>
2023-06-28 22:16:01 +0900

    중간업로드

--- routes/comments.js
+++ routes/comments.js
@@ -2,45 +2,54 @@ const express = require('express');
 const router = express.Router();
 const { Comment, Post, User } = require('../models');
 const { Op } = require('sequelize');
+const middleware = require('../middlewares/middleware.js');
 
 //import * as tweetController from '../controller/tweet.js';
-
 // router.get('/', (req, res, next) => {
 //   res.send('comments router');
 // });
 
 //댓글 생성
 router.post('/', async (req, res) => {
-  const { content, userId } = req.body;
-  // if (!userId) {
-  //   res.status(400).json({
-  //     errorMessage: '로그인이 필요한 기능입니다.',
-  //   });
-  // }
+  // const { postid } = req.params;
+  // const { userid } = req.params;
+  //  const user = await User.findOne({ where: { id: userid } });
 
-  try {
-    const user = await User.findOne({
-      attributes: [`id`, `nickname`, `email`, 'password'],
-      where: { id: userId },
-    });
-    if (!user) {
-      res.status(400).json({
-        errorMessage: '존재하지 않는 사용자 입니다.',
-      });
-    }
-    const comment = await Comment.create({
-      content: content,
-      userId: userId,
-    });
-
-    res.status(200).json({ comment });
-  } catch {
-    // console.error(error);
-    // res.status(500).json({
-    //   errorMessage: '서버 오류',
-    // });
-  }
+  const { content, postid, userid } = req.body;
+  const comment = await Comment.create({
+    content: content,
+  });
+  res.status(200).json({ comment });
 });
+// if (!userId) {
+//   res.status(400).json({
+//     errorMessage: '로그인이 필요한 기능입니다.',
+//   });
+// }
+
+// try {
+//   const user = await User.findOne({
+//     attributes: [`id`, `nickname`, `email`, 'password'],
+//     where: { id },
+//   });
+//   console.log(user);
+//   if (!user) {
+//     res.status(400).json({
+//       errorMessage: '존재하지 않는 사용자 입니다.',
+//     });
+//   }
+// } catch (error) {}
+// const comment = await Comment.create({
+//   content: content,
+//   userId: user.userId,
+// });
+
+// } catch (error) {
+//   console.error(error);
+//   res.status(500).json({
+//     errorMessage: '서버 오류',
+//   });
+// }
 
 //댓글 조회
 router.get('/', async (req, res) => {
@@ -67,13 +76,12 @@ router.put('/:id', async (req, res) => {
     updateAt: Date.now,
   });
 
-  res.status(200).json({ content: content });
+  res.status(200).json({ comment });
 });
 
 //삭제
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const { content } = req.body;
   const comment = await Comment.findOne({ id });
   if (!comment) {
     res.status(400).json({
