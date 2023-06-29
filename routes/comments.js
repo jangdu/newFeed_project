82eacb015b8730e41809d91e9b2384b724be32d8
Lee Eunchan <ym4315@nate.com>
2023-06-29 13:23:38 +0900

    중간업로드

--- routes/comments.js
+++ routes/comments.js
@@ -9,48 +9,6 @@ const middleware = require('../middlewares/middleware.js');
 //   res.send('comments router');
 // });
 
-//댓글 생성
-router.post('/', async (req, res) => {
-  // const { postid } = req.params;
-  // const { userid } = req.params;
-  //  const user = await User.findOne({ where: { id: userid } });
-
-  const { content, postid, userid } = req.body;
-  const comment = await Comment.create({
-    content: content,
-  });
-  res.status(200).json({ comment });
-});
-// if (!userId) {
-//   res.status(400).json({
-//     errorMessage: '로그인이 필요한 기능입니다.',
-//   });
-// }
-
-// try {
-//   const user = await User.findOne({
-//     attributes: [`id`, `nickname`, `email`, 'password'],
-//     where: { id },
-//   });
-//   console.log(user);
-//   if (!user) {
-//     res.status(400).json({
-//       errorMessage: '존재하지 않는 사용자 입니다.',
-//     });
-//   }
-// } catch (error) {}
-// const comment = await Comment.create({
-//   content: content,
-//   userId: user.userId,
-// });
-
-// } catch (error) {
-//   console.error(error);
-//   res.status(500).json({
-//     errorMessage: '서버 오류',
-//   });
-// }
-
 //댓글 조회
 router.get('/', async (req, res) => {
   const comment = await Comment.findAll({});
@@ -66,11 +24,38 @@ router.get('/:id', async (req, res) => {
   res.status(200).json({ comment: comment });
 });
 
+//댓글 생성
+router.post('/', middleware, async (req, res) => {
+  const { content } = req.body;
+  const userId = req.userId;
+
+  if (!content) {
+    res.status(400).json({
+      errorMessage: '댓글을 입력해주세요',
+    });
+  }
+
+  const postId = await Post.findOne({ id: Post.id });
+
+  const comment = await Comment.create({
+    content: content,
+    postId: Post.id,
+    userId: userId,
+  });
+  res.status(200).json({ comment });
+});
+
 //수정
 router.put('/:id', async (req, res) => {
   const { id } = req.params;
   const { content } = req.body;
   const comment = await Comment.findOne({ where: { id } });
+  console.log(comment);
+  if (!comment) {
+    res.status(400).json({
+      errorMessage: '존재하지 않는 댓글입니다.',
+    });
+  }
   await comment.update({
     content: content,
     updateAt: Date.now,
