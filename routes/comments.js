19cfd8142bbb9f889d68d6e29afe2c00eb0cb240
jangdu <jjd0324@gmail.com>
2023-06-30 09:43:58 +0900

    fix: GET /comments 에서 userId -> id로 오타 수정

--- routes/comments.js
+++ routes/comments.js
@@ -17,7 +17,7 @@ router.get('/:postId', async (req, res) => {
     const comment = await Comment.findAll({
       include: {
         model: User,
-        attributes: ['userId', 'nickname'],
+        attributes: ['id', 'nickname'],
       },
       where: { postId },
       order: [['createdAt', 'DESC']],
